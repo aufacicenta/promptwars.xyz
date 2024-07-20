7ba136dab6f06c3f0f484ba855154c89182dadbd
netpoe <gus@aufacicenta.com>
2024-07-20 13:10:57 -0600

    refactor: uses VERCEL_BRANCH_URL

--- src/hooks/useRoutes/useRoutes.tsx
+++ src/hooks/useRoutes/useRoutes.tsx
@@ -1,7 +1,7 @@
 export const origin =
   process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
     ? `${process.env.NEXT_PUBLIC_PROTOCOL_SCHEME}://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
-    : `${process.env.NEXT_PUBLIC_PROTOCOL_SCHEME}://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+    : `${process.env.NEXT_PUBLIC_PROTOCOL_SCHEME}://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`;
 
 export const routes = {
   home: () => `/`,
